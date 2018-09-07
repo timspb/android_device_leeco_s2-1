56e88ae2e3c1b690d3640d99ce9d7e4441d086c4
Prema Chand Alugu <premaca@gmail.com>
2018-09-08 03:03:19 +0530

    s2: cam: Build vendor version for libgui
    * Requires support in frameworks/native
    * https://review.gzospgzr.com/#/c/2179/
    * https://review.arrowos.net/2771

--- device.mk
+++ device.mk
@@ -151,7 +151,8 @@ PRODUCT_PACKAGES += \
     camera.msm8952 \
     libqomx_core \
     libmmcamera_interface \
-    libmmjpeg_interface
+    libmmjpeg_interface \
+    libgui_vendor
 endif
 
 # Connectivity Engine support (CNE)
