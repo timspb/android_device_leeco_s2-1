16eb45b9818a21015c35bd3b54c318901ef2b447
timspb <5390894+timspb@users.noreply.github.com>
2018-12-20 19:04:34 +1000

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=leeco
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+HAVOC_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$HAVOC_ROOT"/vendor/havoc/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -51,7 +51,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$HAVOC_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
